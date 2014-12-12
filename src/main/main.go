e92373c7a262067c4bd7fc641d96d72e8f424729
korotkih <korotkih@corp.sputnik.ru>
2014-12-12 16:56:57 +0400

    countires config added

--- src/main/main.go
+++ src/main/main.go
@@ -7,39 +7,58 @@ import (
 	"jsonParser"
 	"log"
 	"os"
+	"ruleParser"
 	"strconv"
 	//"strings"
-	"sync"
+	//"sync"
 )
 
-var wg sync.WaitGroup
+//var wg sync.WaitGroup
 
 var conffile = flag.String("conf", "", "config file")
+var rulesfile = flag.String("rules", "", "rules file")
 
 func main() {
 	//
 	flag.Parse()
 	conf := configParser.GetConf(*conffile)
+	rules := ruleParser.GetConf(*rulesfile)
+	log.Print(rules["SLV"])
 	//
-	log.Println("porcessing JSONs")
-	files := fileWR.ReadFolder(conf.Folders.Input)
-	for i := conf.Zooms.Min; i <= conf.Zooms.Max; i++ {
-		for j := range files {
-			wg.Add(1)
-			go func(f string, i int) {
-				jsonRaw := fileWR.ReadFile(conf.Folders.Input + "/" + f)
-				jsonS, name := jsonParser.ProcessJSON(jsonRaw, i, conf.Naming.Feature_prop)
+	/*
+		log.Println("porcessing JSONs")
+		files := fileWR.ReadFolder(conf.Folders.Input)
+		for i := conf.Zooms.Min; i <= conf.Zooms.Max; i++ {
+			for j := range files {
+				wg.Add(1)
+				go func(f string, i int) {
+					jsonRaw := fileWR.ReadFile(conf.Folders.Input + "/" + f)
+					jsonS, name := jsonParser.ProcessJSON(jsonRaw, i, conf.Naming.Feature_prop)
+					os.MkdirAll(conf.Folders.Output+"/"+strconv.Itoa(i), 0777)
+					fileWR.WiteFile(conf.Folders.Output+"/"+
+						strconv.Itoa(i)+"/"+name, jsonS)
+					defer func() {
+						wg.Done()
+					}()
+				}(files[j].Name(), i)
+			}
+		}
+		wg.Wait()
+		defer func() {
+			log.Println("processing complete")
+		}()
+	*/
+	//
+	for k, v := range rules {
+		for j := range v {
+			for i := v[j].Range[0]; i <= v[j].Range[1]; i++ {
+				jsonRaw := fileWR.ReadFile(conf.Folders.Input + "/" + v[j].File)
+				jsonS, _ := jsonParser.ProcessJSON(jsonRaw, i, conf.Naming.Feature_prop)
 				os.MkdirAll(conf.Folders.Output+"/"+strconv.Itoa(i), 0777)
 				fileWR.WiteFile(conf.Folders.Output+"/"+
-					strconv.Itoa(i)+"/"+name, jsonS)
-				defer func() {
-					wg.Done()
-				}()
-			}(files[j].Name(), i)
+					strconv.Itoa(i)+"/"+k+".json", jsonS)
+			}
 		}
 	}
-	wg.Wait()
-	defer func() {
-		log.Println("processing complete")
-	}()
+	//
 }
